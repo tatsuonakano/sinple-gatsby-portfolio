af43f47e64ac92bee004a1688c33f8949d670045
T.N <85272779+tatsuoNakano@users.noreply.github.com>
2022-08-30 06:56:22 +0900

    なかなかいい感じにCSSがキマったのでcommit  opacityの挙動がやっかいなので注意が必要

--- src/components/layout/firstSubColumn.jsx
+++ src/components/layout/firstSubColumn.jsx
@@ -11,7 +11,7 @@ const FirstSubColumn = () => (
     <div className="d-none d-sm-block col-sm-4 col-xl-3">
       <div>
 
-        <Accordion className="glass-base glass-white-bg opacity-lv-08" defaultActiveKey={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']} alwaysOpen flush>
+        <Accordion className="glass-base glass-white-bg bootstrap-opacity" defaultActiveKey={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']} alwaysOpen flush>
 
           <Accordion.Item eventKey="0">
             <Accordion.Header >
