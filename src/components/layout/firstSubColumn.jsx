a09e13c9f4be37d9b24bd33fa99958eff673f6aa
T.N <85272779+tatsuoNakano@users.noreply.github.com>
2022-09-01 12:49:22 +0900

    サイドバーメニューカスタマイズ

--- src/components/layout/firstSubColumn.jsx
+++ src/components/layout/firstSubColumn.jsx
@@ -10,7 +10,7 @@ const FirstSubColumn = () => (
   <>
     <div className="d-none d-sm-block col-sm-4 col-xl-3">
 
-        <Accordion className="glass-base glass-white-bg bootstrap-opacity fs-6 fw-bold" defaultActiveKey={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']} alwaysOpen flush>
+        <Accordion className="glass-base glass-white-bg bootstrap-opacity fs-6 fw-bold" defaultActiveKey={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10','11','12']} alwaysOpen flush>
 
           <Accordion.Item eventKey="0">
             <Accordion.Header >
@@ -115,7 +115,7 @@ const FirstSubColumn = () => (
 
           <Accordion.Item eventKey="8">
             <Accordion.Header>
-              <span className="accordionHeaderStyle"> :カテゴリー
+              <span className="accordionHeaderStyle">D3.js
               </span>
             </Accordion.Header>
             <Accordion.Body>
@@ -125,11 +125,77 @@ const FirstSubColumn = () => (
 
           <Accordion.Item eventKey="9">
             <Accordion.Header>
-              <span className="accordionHeaderStyle"> :カテゴリー
+              <span className="accordionHeaderStyle">MUI(material-ui)
               </span>
             </Accordion.Header>
             <Accordion.Body>
               <Link to="/#">#</Link>
+            </Accordion.Body>
+          </Accordion.Item>
+          
+          <Accordion.Item eventKey="10">
+            <Accordion.Header>
+              <span className="accordionHeaderStyle">React-Bootstrap
+              </span>
+            </Accordion.Header>
+            <Accordion.Body>
+              <Link to="/#">#</Link>
+            </Accordion.Body>
+          </Accordion.Item>
+          
+          <Accordion.Item eventKey="11">
+            <Accordion.Header>
+              <span className="accordionHeaderStyle">TailwindCSS
+              </span>
+            </Accordion.Header>
+            <Accordion.Body>
+              <Link to="/#">#</Link>
+            </Accordion.Body>
+          </Accordion.Item>
+          
+          <Accordion.Item eventKey="12">
+            <Accordion.Header>
+              <span className="accordionHeaderStyle">React ライブラリ
+              </span>
+            </Accordion.Header>
+            <Accordion.Body>
+              <Link to="/#">React Loader Spinner</Link>
+            </Accordion.Body>
+            <Accordion.Body>
+              <Link to="/#">Video React</Link>
+            </Accordion.Body>
+            <Accordion.Body>
+              <Link to="/#">React Typed</Link>
+            </Accordion.Body>
+            <Accordion.Body>
+              <Link to="/#">React Animations</Link>
+            </Accordion.Body>
+            <Accordion.Body>
+              <Link to="/#">React Reveal</Link>
+            </Accordion.Body>
+            <Accordion.Body>
+              <Link to="/#">react beautiful dnd</Link>
+            </Accordion.Body>
+            <Accordion.Body>
+              <Link to="/#">Recharts</Link>
+            </Accordion.Body>
+            <Accordion.Body>
+              <Link to="/#">Visx</Link>
+            </Accordion.Body>
+            <Accordion.Body>
+              <Link to="/#">React StockCharts</Link>
+            </Accordion.Body>
+            <Accordion.Body>
+              <Link to="/#">Material Table</Link>
+            </Accordion.Body>
+            <Accordion.Body>
+              <Link to="/#">React PDF</Link>
+            </Accordion.Body>
+            <Accordion.Body>
+              <Link to="/#">React Awesome Slider</Link>
+            </Accordion.Body>
+            <Accordion.Body>
+              <Link to="/#">React Image Gallery</Link>
             </Accordion.Body>
           </Accordion.Item>
 
