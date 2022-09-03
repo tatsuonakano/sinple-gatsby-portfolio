2cd801986ac5430b6c0e5d8bd3a8d434a97905cc
T.N <85272779+tatsuoNakano@users.noreply.github.com>
2022-09-03 12:23:37 +0900

    ビルド成功 SSR時にはSSRProviderコンポーネントで包むようにって警告が出たが、SSGしか利用しないので今は無視する。対応が必要になった時に考える

---
+++ src/pages/inquiry.js
@@ -0,0 +1,23 @@
+import * as React from "react"
+import Layout from "../components/layout/layout"
+import { Link } from "gatsby"
+
+
+const Inquiry = () => {
+  return (
+<Layout>
+<hr/>
+<h1>問い合わせ</h1>
+
+
+
+<hr/>
+<Link to="/">サイトトップへ戻る</Link>
+
+<hr/>
+
+</Layout>
+)
+}
+
+export default Inquiry
